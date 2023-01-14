a78a1837115d4d3d021474f6a6ebe26277a912e1
Beppe Catanese <gcatanese@yahoo.com>
2023-01-14 14:28:12 +0100

    Refactor tests

--- src/test/java/com/tweesky/cloudtools/OpenApiValidatorTest.java
+++ src/test/java/com/tweesky/cloudtools/OpenApiValidatorTest.java
@@ -4,26 +4,59 @@ import com.tweesky.cloudtools.schema.SchemaUtil;
 import com.tweesky.cloudtools.validator.OpenApiValidator;
 import com.tweesky.cloudtools.validator.OpenApiValidatorObject;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class OpenApiValidatorTest {
 
+    private final Logger log = LoggerFactory.getLogger(OpenApiValidatorTest.class);
+
+
     @Test
     public void validate() throws IOException {
-        String schema = "src/test/resources/schema/ManagementService-v1.json";
-
-        String path = "/companies/{{companyId}}/webhooks";
-        String response = "src/test/resources/json/mgmt-api-response.json";
+        String schema = "src/test/resources/schema/BasicJson.json";
 
         OpenApiValidator validator = new OpenApiValidator(SchemaUtil.getContent(schema));
 
-        validator.validate(OpenApiValidatorObject.forMethod("GET")
-                .withPath(path)
-                .withResponseBody(SchemaUtil.getContent(response))
+        var responseData = validator.validate(OpenApiValidatorObject.forMethod("GET")
+                .withPath("/users/1")
+                .withHeaders(new ArrayList<>())
+                .withResponseBody(getUserResponseBody())
                 .withResponseContentType("application/json")
                 .withStatus(200)
         );
+
+        assertTrue(responseData.isValid());
+    }
+
+    @Test
+    public void validateFail() throws IOException {
+        String schema = "src/test/resources/schema/BasicJson.json";
+
+        OpenApiValidator validator = new OpenApiValidator(SchemaUtil.getContent(schema));
+
+        var responseData = validator.validate(OpenApiValidatorObject.forMethod("POST")
+                .withPath("/users/1")
+                .withHeaders(new ArrayList<>())
+                .withResponseBody(getUserResponseBody())
+                .withResponseContentType("application/json")
+                .withStatus(200)
+        );
+
+        assertFalse(responseData.isValid());
+    }
+
+    private String getUserResponseBody() {
+        return "{" +
+                " \"id\": 1," +
+                " \"firstName\": \"Beppe\"," +
+                " \"lastName\": \"Catanese\"" +
+                "}";
 
     }
 
